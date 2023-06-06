c578867a1a942f08783cb1bc867a1e2ef0aacd47
BhaskarSingh10 <bhaskarsandhu7612@gmail.com>
2023-06-06 18:07:25 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="email-logo-png-1119.png" type="image/x-icon">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Email Subscription Form</title>
+</head>
+<body>
+
+    <div class="container">
+        <h1>Subscribe to our Newsletter</h1>
+        <form action="subscribe.php" method="get"></form>
+        <div class="email">
+            <input type="email" placeholder="Email">
+            <i class="icon fa fa-envelope"></i>
+            <button type="submit">Subscribe</button>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ subscribe.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Subscribe to Newsletter</title>
+		<meta name="author" content="The name of the document’s author to go here" />
+		<meta name="description" content="A concise and accurate summary of the document content is to appear here">
+		<link rel="shortcut icon" href="email-logo-png-1119.png" type="image/x-icon" />
+		<link rel="stylesheet" href="styles.css" />
+	</head>
+	<body>
+		<h1>Thanku for Subscribing<h1>
+		<?php $emailAddress = $_GET['emailAddress'];
+		echo
+		 "<p>Your email address, $emailAddress, has been added to our list of subscribers.</p>" </?php>
+		
+
+
+		 </body>
+</html>
