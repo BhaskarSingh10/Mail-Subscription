05d6e5720929ed675e157c3529b9460a31d9fb7c
BhaskarSingh10 <135094363+BhaskarSingh10@users.noreply.github.com>
2023-06-09 14:43:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,24 +2,23 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="email-logo-png-1119.png" type="image/x-icon">
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="php" href="subscribe.php">
     <title>Email Subscription Form</title>
 </head>
 <body>
-
-    <div class="container">
-        <h1>Subscribe to our Newsletter</h1>
-        <form action="subscribe.php" method="get"></form>
-        <div class="email">
-            <input type="email" placeholder="Email">
-            <i class="icon fa fa-envelope"></i>
+    <form action="subscribe.php" method="get">
+        <h1>Subscribe to Newsletter</h1>
+        <label for="emailAddress">Email Address:</label>
+        <label value="id" id="emailaddress"></label>
+         <input name="email" type="email" id="emailaddress" required>
             <button type="submit">Subscribe</button>
-        </div>
-    </div>
-    
+       </form>
+       <footer>
+        <p>@e-Mail Subscription Inc.</p>
+    </footer>
+       
 </body>
 </html>
