bc0271232cafeb34646edaa966980374270952c0
BhaskarSingh10 <135094363+BhaskarSingh10@users.noreply.github.com>
2023-06-10 01:29:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="shortcut icon" href="email-logo-png-1119.png" type="image/x-icon">
-    <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="https://bhaskarsingh10.github.io/Mail-Subscription/">
-    <title>Email Subscription Form</title>
-</head>
-<body>
-    <aside>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>E-Mail Subscription Form</title>
+        <link rel="stylesheet" href="styles.css">
+        <link src="shortcut icon" href="email-logo-png-1119.png">
+    </head>
+    <body>
+        <div>
+        <header>Subscribe To Newsletter</header>
+    </div>
     <form action="index.php" method="POST">
-        <h3>Subscribe to Newsletter</h3>
-        <label for="emailAddress">Email Address:</label>
-        <label value="id" id="emailaddress"></label>
-         <input name="email" type="email" id="emailaddress" required>
-            <button type="submit">Subscribe</button>
-        </form>
-    </aside>
-       <footer>
-        <p>© Email Subscription Inc.</p>
-    </footer>
-       
-</body>
+
+        <?php
+        $userEmail = "";
+        if (isset($_POST["subscribe"])) {
+            $userEmail = $_POST["email"]; // Fix: Use square brackets instead of curly braces for accessing POST data
+            $subject = "Thank You for Subscribing"; // Moved the variable assignment outside the echo statement
+            $message = "Thank You for Subscribing"; // Moved the variable assignment outside the echo statement
+            // Code for sending the email goes here
+        }
+        ?>
+
+        <div class="field">
+            <input type="text" placeholder="Email Address" required>
+ </div>
+        <div class="field BUTTON">
+            <input type="submit" name="subscribe" value="Subscribe">
+         </div>
+              
+    </form>
+
+    </body>
 </html>
